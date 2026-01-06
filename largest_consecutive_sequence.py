7092498b2ef79f09a0e18742b0e7d1ac34d4124f
Kehinde <giwatwins@gmail.com>
2026-01-05 22:51:23 -0800

    optimized longest consecutive sequence and solved two sum

--- largest_consecutive_sequence.py
+++ largest_consecutive_sequence.py
@@ -1,18 +1,35 @@
 numsa = [2,20,4,10,3,4,5]
  
  
+# def longest_consecutive(nums):
+#     nums_map = {}
+#     length=0
+#     for num in nums:
+#         if num not in nums_map:
+#             nums_map[num] = True
+#     for num in nums:
+#         next = num
+#         temp_length = 1
+#         while next+1 in nums_map:
+#             temp_length +=1
+#             next+=1
+#         length = max(length,temp_length)
+#     return length 
+
+
 def longest_consecutive(nums):
     nums_map = {}
     length=0
     for num in nums:
         if num not in nums_map:
             nums_map[num] = True
-    for num in nums:
+    for num in nums_map:
         next = num
         temp_length = 1
-        while next+1 in nums_map:
-            temp_length +=1
-            next+=1
-        length = max(length,temp_length)
+        if num-1 not in nums_map:
+            while next+1 in nums_map:
+                temp_length +=1
+                next+=1
+            length = max(length,temp_length)
     return length 
 print(longest_consecutive(numsa))
---
+++ two_sum_II.py
@@ -0,0 +1,18 @@
+numbers = [1,2,3,4]
+target = 3
+
+def two_sum(numbers,target):
+    left = 0
+    right = len(numbers)-1
+    
+    while left< right:
+        if numbers[left] + numbers[right] == target:
+            return [left+1,right+1]
+        elif numbers[left] + numbers[right] < target:
+            left+=1
+        elif numbers[left] + numbers[right] > target:
+            right-=1
+    
+       
+
+print(two_sum(numbers,target))
