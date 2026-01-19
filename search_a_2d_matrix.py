2c292cf223e2a7bd6740e9e52e573980b53f51e1
Kehinde <giwatwins@gmail.com>
2026-01-19 12:20:57 +0100

    fix: fixed the bug lol

--- search_a_2d_matrix.py
+++ search_a_2d_matrix.py
@@ -1,18 +1,22 @@
-matrix = [[1, 2, 4, 8], [10, 11, 12, 13], [14, 20, 30, 40]]
+# matrix = [[1, 2, 4, 8], [10, 11, 12, 13], [14, 20, 30, 40]]
+matrix = [[1]]
 target = 10
 
 
 def searchMatrix(matrix, target):
-
     i = 0
 
     while i < len(matrix):
+
         if matrix[i][-1] == target:
             return True
-        if matrix[i][-1] < target:
+        elif matrix[i][-1] < target:
             i += 1
-        if matrix[i][-1] > target:
+        elif matrix[i][-1] > target:
             break
+
+    if i == len(matrix):
+        return False
 
     left, right = 0, len(matrix[i]) - 1
     while left <= right:
